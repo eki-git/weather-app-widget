862a6234be93e56ed778f41b30b7dcc83ff73e7a
eki-git <eramcilovic@gmail.com>
2025-07-08 11:20:07 +0200

    initial setup

--- src/js/Components/WeatherWidget/Widget.js
+++ src/js/Components/WeatherWidget/Widget.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Widget.scss";
+import Header from "./Header";
 const Widget = (props) => {
     return (
         <section className="widget__section">
-
+          <Header></Header>
         </section>
     )
 }
